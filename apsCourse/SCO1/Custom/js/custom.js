6f128c06e1dc45eb8a1ea16777d9c36ed40d18d5
dmp569 <44303380+dmp569@users.noreply.github.com>
2018-10-19 15:26:46 -0400

    initial build
    Building the original

---
+++ apsCourse/SCO1/Custom/js/custom.js
@@ -0,0 +1,9 @@
+/**************************************************************************
+** NOTE TO COURSE DEVELOPERS:
+** -- Any custom JavaScript should be placed in this file.
+** -- Changes to course.js or other code provided as part of
+**    this SDK is strictly prohibited.
+** -- JavaScript in content .htm files should be limited to event handlers
+**   (e.g. onClick, onMouseEnter, etc.) which call functions in this file.
+** -- No custom JavaScript script blocks should be in content .htm files.
+**************************************************************************/
